1ae609b16a94b139611f27a798cd5529c49a8006
BasvBo <37073686+BasvBo@users.noreply.github.com>
2018-03-09 09:52:26 +0100

    interface klaar

---
+++ Basinterface/src/main/java/nl/Basinterface/RekenenMetBas.java
@@ -0,0 +1,10 @@
+package nl.Basinterface;
+
+public interface RekenenMetBas {
+
+    // optellen van twee waardes
+    int addingup(int a, int b);
+
+    //vermenigvuldigenvan twee waardes
+    int multiply(int c, int d);
+}
